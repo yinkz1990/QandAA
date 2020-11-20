f7d5514adeed23cc68189928d24678cb6184a542
Idumu Olayinka Osayame <43887633+yinkz1990@users.noreply.github.com>
2020-11-21 00:28:28 +0100

    Add project files.

---
+++ Data/DataRepository.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using Dapper;
+using QandAA.Data.Models;
+
+namespace QandAA.Data
+{
+    public class DataRepository : IDataRepository
+    {
+        private readonly string _connectionString;
+        public DataRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration["ConnectionStrings:DefaultConnection"];
+
+        }
+
+        public AnswerGetResponse GetAnswer(int answerId)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                return connection.QueryFirstOrDefault<AnswerGetResponse>(
+                @"EXEC dbo.Answer_Get_ByAnswerId @AnswerId = @AnswerId",
+                new { AnswerId = answerId }
+                );
+            }
+        
+            throw new NotImplementedException();
+        }
+
+        public QuestionGetSingleResponse GetQuestion(int questionId)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var question = connection.QueryFirstOrDefault<QuestionGetSingleResponse>(@"EXEC dbo.Question_GetSingle @QuestionId = @QuestionId",
+                    new { QuestionId = questionId });
+
+                if (question != null)
+                {
+                    question.Answers = connection.Query<AnswerGetResponse>(@"EXEC dbo.Answer_Get_ByQuestionId @QuestionId = @QuestionId",
+                        new { QuestionId = questionId });
+                }
+
+                return question;
+            }
+                throw new NotImplementedException();
+        }
+
+        public IEnumerable<QuestionGetManyResponse> GetQuestions()
+        {
+            using (var connection = new SqlConnection(_connectionString)){
+                connection.Open();
+                return connection.Query<QuestionGetManyResponse>(@"EXEC dbo.Question_GetMany");
+            }
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<QuestionGetManyResponse> GetQuestionsBySearch(string search)
+        { 
+            using(var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                return connection.Query<QuestionGetManyResponse>(@"EXEC dbo.Question_GetMany_BySearch @Search = @Search", new { Search = search });
+            }
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<QuestionGetManyResponse> GetUnansweredQuestions()
+        {   
+            using(var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                return connection.Query<QuestionGetManyResponse>(@"EXEC dbo.Question_GetUnanswered");
+            }
+            throw new NotImplementedException();
+        }
+
+        public bool QuestionExists(int questionId)
+        {
+
+            using(var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                return connection.QueryFirst<bool>(@"EXEC dbo.Question_Exists @QuestionId = @QuestionId", new { QuestionId = questionId });
+            }
+            throw new NotImplementedException();
+        }
+
+        public QuestionGetSingleResponse PostQuestion(QuestionPostRequest question)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var questionId = connection.QueryFirst<int>( @"EXEC dbo.Question_Post @Title = @Title, 
+                    @Content = @Content,@UserId = @UserId, @UserName = @UserName, @Created = @Created", question );
+                return GetQuestion(questionId);
+            }
+        }
+
+        public QuestionGetSingleResponse PutQuestion(int questionId, QuestionPutRequest question)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                connection.Execute(
+                @"EXEC dbo.Question_Put @QuestionId = @QuestionId, @Title = @Title, @Content = @Content",
+                new { QuestionId = questionId, question.Title, question.Content });
+                return GetQuestion(questionId);
+            }
+        }
+
+        public void DeleteQuestion(int questionId)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                connection.Execute(
+                @"EXEC dbo.Question_Delete @QuestionId = @QuestionId",
+                new { QuestionId = questionId }
+                );
+            }
+        }
+
+        public AnswerGetResponse PostAnswer(AnswerPostRequest answer)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                return connection.QueryFirst<AnswerGetResponse>(
+                @"EXEC dbo.Answer_Post @QuestionId = @QuestionId, @Content = @Content, 
+@UserId = @UserId, @UserName = @UserName, @Created = @Created", answer
+                );
+            }
+        }
+    }
+}
---
+++ Data/IDataRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using QandAA.Data.Models;
+
+namespace QandAA.Data
+{
+    public interface IDataRepository
+    {
+
+        IEnumerable<QuestionGetManyResponse> GetQuestions();
+
+        IEnumerable<QuestionGetManyResponse> GetQuestionsBySearch(string search);
+       
+        IEnumerable<QuestionGetManyResponse> GetUnansweredQuestions();
+        
+        QuestionGetSingleResponse GetQuestion(int questionId);
+        
+        bool QuestionExists(int questionId);
+        
+        AnswerGetResponse GetAnswer(int answerId);
+
+        QuestionGetSingleResponse PostQuestion(QuestionPostRequest question);
+
+        QuestionGetSingleResponse PutQuestion(int questionId, QuestionPutRequest question);
+
+        void DeleteQuestion(int questionId);
+
+        AnswerGetResponse PostAnswer(AnswerPostRequest answer);
+    }
+}
+
